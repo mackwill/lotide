5c85f428c8da9ac6f2ffcbda40feb368c8bd88ce
William <mack.mindenhall@gmail.com>
2020-06-22 19:28:08 +0000

    Added custom assert function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = (actual, expected) => {
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//Test Code
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 1);
