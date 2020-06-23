26fc52aacae93e410de513a9ff9d07ab75595b64
William <mack.mindenhall@gmail.com>
2020-06-23 17:35:51 +0000

    Added eqArrays which returns whether two arrays are equal or not

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual = (actual, expected) => {
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (firstArray, secondArray) => {
+  const boolFlag = firstArray.every((currentElem) => {
+    return currentElem === secondArray[firstArray.indexOf(currentElem)];
+  });
+  return boolFlag;
+};
+
+// Code Tests
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
