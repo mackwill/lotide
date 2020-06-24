a6058511e3e6944ac70040ffbd9f21e29b5d5791
William <mack.mindenhall@gmail.com>
2020-06-24 11:38:37 +0000

    Added countLetter.js function which counts the number of occurances of each letter in a provided string.

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = (actual, expected) => {
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = (strToSearch) => {
+  let foundLetters = {};
+
+  for (let char of strToSearch.split(" ").join("").toLowerCase()) {
+    if (foundLetters.hasOwnProperty(char)) {
+      foundLetters[char]++;
+    } else {
+      foundLetters[char] = 1;
+    }
+  }
+
+  return foundLetters;
+};
+
+// Test Code
+// let results = countLetters("LHL");
+// assertEqual(results["l"], 2);
+// assertEqual(results["h"], 1);
+
+// let results = countLetters("lighthouse in the house");
+// assertEqual(results["l"], 1);
+// assertEqual(results["i"], 2);
+// assertEqual(results["g"], 1);
+// assertEqual(results["h"], 4);
+// assertEqual(results["t"], 2);
+// assertEqual(results["o"], 2);
+// assertEqual(results["u"], 2);
+// assertEqual(results["s"], 2);
+// assertEqual(results["e"], 3);
+// assertEqual(results["n"], 1);
