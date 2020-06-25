2498476b90c0998476eca737cfcbdf55750b2514
William <mack.mindenhall@gmail.com>
2020-06-25 11:58:49 +0000

    Added takeUntil.js which returns an array that contains values starting from the beginning that pass a given condition

---
+++ takeUntil.js
@@ -0,0 +1,12 @@
+const takeUntil = function (array, callback) {
+  let results = [];
+
+  for (let elem of array) {
+    if (callback(elem)) {
+      return results;
+    } else {
+      results.push(elem);
+    }
+  }
+  return results;
+};
